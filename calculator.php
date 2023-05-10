ed8502e38d380c17493f22ec60cca3465e87d40b
Josh-ByteCode <joshua.lauurence.fabi@gmail.com>
2023-05-10 21:17:10 +0800

    changer in calculator

--- calculator.php
+++ calculator.php
@@ -59,12 +59,30 @@ if(isset($_SESSION['user_id'])){
       </div>
 </div>
 <br>
+<div class="result-container">
+  <div class="result-form-container">
+    <form action="">
+      <div class="itemListResult" id="itemList"></div>
+    </form>
+  </div>
+</div>
 <br>
 <?php include 'components/footer.php'; ?>
 <script src="js/script.js"></script>
 <script>
   const projectTypeSelect = document.getElementById("projectCategory");
   const subCategorySelect = document.getElementById("subCategory");
+  const itemListDiv = document.getElementById("itemList");
+  const calculateBtn = document.getElementById("calculate");
+  const resultContainer = document.querySelector(".result-container");
+
+  // Event listener for when the calculate button is clicked
+  calculateBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Show the result container
+    resultContainer.style.display = "block";
+  });
 
   // Define the sub categories for each project type
   const subCategories = {
@@ -72,14 +90,24 @@ if(isset($_SESSION['user_id'])){
     building: ["Hotel", "Office Building", "Retail Space"]
   };
 
+  // Define the list of items for each sub category
+  const subCategoryItems = {
+    "Bathroom Renovation": ["Tiles", "Sand", "Gravel", "Cement"],
+    "Kitchen Renovation": ["Countertops", "Cabinets", "Sink", "Faucet"],
+    "Room Addition": ["Lumber", "Drywall", "Insulation", "Paint"],
+    "Hotel": ["Concrete", "Steel", "Glass", "Furniture"],
+    "Office Building": ["Brick", "Aluminum", "Wood", "Computers"],
+    "Retail Space": ["Flooring", "Lighting", "Shelving", "Merchandise"]
+  };
+
   // Event listener for when a project type is selected
   projectTypeSelect.addEventListener("change", (event) => {
     // Clear the sub category select options
     subCategorySelect.innerHTML = "";
-    
+
     // Get the selected project type value
     const projectCategory = event.target.value;
-    
+
     // Create a new option for each sub category
     if (projectCategory) {
       subCategories[projectCategory].forEach((subCategory) => {
@@ -88,12 +116,47 @@ if(isset($_SESSION['user_id'])){
         option.text = subCategory;
         subCategorySelect.add(option);
       });
-      
+
       // Show the sub category select
       subCategorySelect.style.display = "block";
     } else {
       // Hide the sub category select if no project type is selected
       subCategorySelect.style.display = "none";
+    }
+  });
+
+  // Event listener for when the calculate button is clicked
+  document.getElementById("calculateBtn").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const selectedSubCategory = subCategorySelect.value;
+
+    if (selectedSubCategory) {
+      const itemsList = subCategoryItems[selectedSubCategory];
+
+      // Clear the previous items list
+      itemListDiv.innerHTML = "";
+
+      if (itemsList && itemsList.length > 0) {
+        const heading = document.createElement("h2");
+        heading.textContent = "Items for " + selectedSubCategory;
+        itemListDiv.appendChild(heading);
+
+        const itemList = document.createElement("ul");
+        itemsList.forEach((item) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = item;
+          itemList.appendChild(listItem);
+        });
+
+        itemListDiv.appendChild(itemList);
+      }
+
+      // Show the items list
+      itemListDiv.style.display = "block";
+    } else {
+      // Hide the items list if no sub category is selected
+      itemListDiv.style.display = "none";
     }
   });
 </script>
