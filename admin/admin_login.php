70bba32166547c1492ee522ddfb6bc1c6b8afed8
Joshua Laurence <128726316+Josh-ByteCode@users.noreply.github.com>
2023-05-14 12:03:11 +0800

    minor changes

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -24,11 +24,8 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Admin - Login</title>
-
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
    <link rel="stylesheet" href="../css/admin_style.css">
-
 </head>
 <body>
 <?php
